96582079f9122a4c054553673f333f67f7d9170b
Julio Rivas <julioandresrivas@gmail.com>
2022-07-07 20:17:35 -0300

    Delete JavaScript directory

--- JavaScript/js/eliminar-paciente.js
+++
@@ -1,8 +0,0 @@
-var tabla = document.querySelector("#tabla-pacientes");
-
-tabla.addEventListener('dblclick',function(event){  
-    event.target.parentNode.classList.add("eliminar-paciente");
-    setTimeout(() => {
-        event.target.parentNode.remove();
-    }, 500);
-});
